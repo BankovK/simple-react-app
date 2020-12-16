0ab29d5cdda4eca8654bed0fbfc214b5f192d879
unknown <KBankou@gomel.iba.by>
2020-12-16 11:31:04 +0300

    section2-unit9

--- src/index.js
+++ src/index.js
@@ -4,20 +4,22 @@ import './index.css';
 
 const useState = React.useState
 
-const pets = [
-    { name: "Meowsalot", species: "cat", age: "5", id: 123456789 },
-    { name: "Barksalot", species: "dog", age: "3", id: 987654321 },
-    { name: "Fluffy", species: "rabbit", age: "2", id: 123123123 },
-    { name: "Purrsloud", species: "cat", age: "1", id: 456456456 },
-    { name: "Paws", species: "dog", age: "6", id: 789789789 }
-]
+function Section1Unit9App() {
 
-function Section1Unit8App() {
+    const [pets, setPets] = useState([
+        { name: "Meowsalot", species: "cat", age: "5", id: 123456789 },
+        { name: "Barksalot", species: "dog", age: "3", id: 987654321 },
+        { name: "Fluffy", species: "rabbit", age: "2", id: 123123123 },
+        { name: "Purrsloud", species: "cat", age: "1", id: 456456456 },
+        { name: "Paws", species: "dog", age: "6", id: 789789789 }
+    ])
+
     return (
         <>
             <Header />
             <LikeClicker />
             <Time />
+            <AddPetForm setPets={setPets}/>
             <ul>
                 {pets.map(
                     pet => <Pet name={pet.name} species={pet.species} age={pet.age} key={pet.id}/>
@@ -25,6 +27,32 @@ function Section1Unit8App() {
             </ul>
             <Footer />
         </>
+    )
+}
+
+function AddPetForm(props) {
+    const [name, setName] = useState()
+    const [species, setSpecies] = useState()
+    const [age, setAge] = useState()
+
+    function handleSubmit(e) {
+        e.preventDefault()
+        props.setPets( prev => prev.concat({name, species, age, id: Date.now()}) )
+        setName("")
+        setSpecies("")
+        setAge("")
+    }
+
+    return (
+        <form onSubmit={handleSubmit}>
+            <fieldset>
+            <legend>Add New Pet</legend>
+            <input value={name} onChange={ e => setName(e.target.value) } placeholder="Name" />
+            <input value={species} onChange={ e => setSpecies(e.target.value) } placeholder="species" />
+            <input value={age} onChange={ e => setAge(e.target.value) } placeholder="age in years" />
+            <button>Add Pet</button>
+            </fieldset>
+        </form>
     )
 }
 
@@ -71,4 +99,4 @@ function Footer() {
     return <small>Footer Text</small>
 }
 
-ReactDOM.render(<Section1Unit8App />, document.getElementById('root'))
+ReactDOM.render(<Section1Unit9App />, document.getElementById('root'))
