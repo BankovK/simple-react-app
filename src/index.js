df1187582a5577a27cffed384976842e1bfb18cf
unknown <KBankou@gomel.iba.by>
2020-12-16 10:28:36 +0300

    section2-unit3

--- src/index.js
+++ src/index.js
@@ -1,14 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import './index.css';
 
-function Section1Unit2App() {
-    return React.createElement("div", null, [
-        React.createElement("h1", null, "Application Header"),
-        React.createElement("p", null, `The current time is ${new Date().toLocaleString()}.`),
-        React.createElement("small", null, "Footer Text"),
-    ])
+function Section1Unit3App() {
+    return (
+        <>
+            <h1 className="special">Application Header</h1>
+            <p>The current time is {new Date().toLocaleString()}.</p>
+            <small>Footer Text</small>
+        </>
+    )
 }
 
 setInterval(() => {
-    ReactDOM.render(React.createElement(Section1Unit2App), document.getElementById('root'))
+    ReactDOM.render(<Section1Unit3App />, document.getElementById('root'))
 }, 1000);
