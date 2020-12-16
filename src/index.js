6c0058577506914b2dd6317b02cbb439880e5e21
unknown <KBankou@gomel.iba.by>
2020-12-16 11:24:52 +0300

    section2-unit5

--- src/index.js
+++ src/index.js
@@ -2,11 +2,16 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 
-function Section1Unit4App() {
+function Section1Unit5App() {
     return (
         <>
             <Header />
             <Time />
+            <ul>
+                <Pet name="Fluffy" species="dog" age="3"/>
+                <Pet name="Cutter" species="cat" age="1"/>
+                <Pet name="Yago" species="parrot" age="10"/>
+            </ul>
             <Footer />
         </>
     )
@@ -20,10 +25,14 @@ function Time() {
     return <p>The current time is {new Date().toLocaleString()}.</p>
 }
 
+function Pet(props) {
+    return <li>{props.name} is a {props.species} and is {props.age} years old.</li>
+}
+
 function Footer() {
     return <small>Footer Text</small>
 }
 
 setInterval(() => {
-    ReactDOM.render(<Section1Unit4App />, document.getElementById('root'))
+    ReactDOM.render(<Section1Unit5App />, document.getElementById('root'))
 }, 1000);
